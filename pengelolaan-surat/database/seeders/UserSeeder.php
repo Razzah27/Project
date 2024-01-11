fb47caef4fb6f3264d146c49775d2a05c6b42aa6
Razzah Decky Satia <razzahdeckysatia@smkwikrama.sch.id>
2024-01-11 22:43:36 +0700

    TU

---
+++ pengelolaan-surat/database/seeders/UserSeeder.php
@@ -0,0 +1,38 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+use App\Models\User;
+
+class DatabaseSeeder extends Seeder
+{
+    /**
+     * Seed the application's database.
+     */
+    public function run(): void
+    {
+        
+        User::create([
+            "name" => "staff",
+            "email" => "staff@gmail.com",
+            "password" => Hash::make("staff"), 
+            "role" => "staff",
+        ]);
+
+        User::create([
+            "name" => "guru",
+            "email" => "guru@gmail.com",
+            "password" => Hash::make("guru"), 
+            "role" => "guru",
+        ]);
+
+        User::create([
+            "name" => "siswa",
+            "email" => "siswa@gmail.com",
+            "password" => Hash::make("siswa"),
+            "role" => "staff",
+        ]);
+    }
+}
